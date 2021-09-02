2520a9cc0fa02c17f88f0ef813abdfe69b505fe6
Zach Steenhoek <zach.steenhoek@my.simpson.edu>
2021-09-02 15:24:50 -0500

    Lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("Dr. Craven said, \"Create at least one escape code!\"")
+print("I think that I am going to love this class!!!")
+print("I am \ntesting\nmy limits!")
+print("Simon said, \"How do I do this?\"")
