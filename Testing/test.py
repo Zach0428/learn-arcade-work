bd8402120418c064e56a9f7299d9ade1c7072b94
Zach Steenhoek <zach.steenhoek@my.simpson.edu>
2021-09-02 15:18:39 -0500

    Lab 1

--- Testing/test.py
+++ Testing/test.py
@@ -1,6 +1,4 @@
-print("""Holy cow!
-The professor
-has
-gone
-crazy!
-""")
+print("Dr. Craven said, \"Create at least one escape code!\"")
+print("I think that I am going to love this class!!!")
+print("I am \ntesting\nmy limits!")
+print("Simon said, \"How do I do this?\"")
